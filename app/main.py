68e11fc6c973a1030981bd82b5ef726cf15d1f2a
Andrey <andreypomortsev@gmail.com>
2025-06-23 20:53:33 +0700

    refactoring

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.api.v1.api import api_router
 from app.core.config import settings
+from app.core.logging_config import setup_logging
 from app.db.base import Base
 from app.db.session import engine
 
@@ -10,12 +13,17 @@ def create_tables():
     Base.metadata.create_all(bind=engine)
 
 
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    if not settings.TESTING:
+        setup_logging()
+        create_tables()
+    yield
 
 
-@app.on_event("startup")
-def on_startup():
-    create_tables()
+app = FastAPI(
+    title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION, lifespan=lifespan
+)
 
 
 app.include_router(api_router, prefix="/api/v1")
