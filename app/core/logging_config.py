c226ac84a40cd50657dd3b9e64ffcbf8e19a0577
Andrey <andreypomortsev@gmail.com>
2025-06-23 20:50:56 +0700

    Added logging

---
+++ app/core/logging_config.py
@@ -0,0 +1,28 @@
+import logging.config
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+        },
+    },
+    "root": {
+        "level": "INFO",
+        "handlers": ["console"],
+    },
+}
+
+
+def setup_logging():
+    """
+    Setup logging configuration.
+    """
+    logging.config.dictConfig(LOGGING_CONFIG)
